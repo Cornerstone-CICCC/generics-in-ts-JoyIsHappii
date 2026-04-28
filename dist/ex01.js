f892a521a05098df4dc6f56c29b867ab1bb6e06b
JoyIsHappii <chayaninkosol@gmail.com>
2026-04-28 14:53:28 -0700

    ex01: done

--- dist/ex01.js
+++ dist/ex01.js
@@ -1,17 +1,18 @@
+"use strict";
 // Create a function called concatSum that takes two generic arguments.
 // These arguments could be strings or numbers. If both are strings, use the concat method; otherwise, just sum the numbers.
 // If they are not of the same type, print an error message to the console; If they are not string or number, print an error message
-
-const concatSum = ( a: string | number, b: string | number) => {
-  if (typeof a === "string" && typeof b === "string") {
-    return a.concat(b);
-  } else if (typeof a === "number" && typeof b === "number") {
-    return a + b;
-  } else {
-    return ("Arguments must be of the same type and either strings or numbers.");
-  }
+const concatSum = (a, b) => {
+    if (typeof a === "string" && typeof b === "string") {
+        return a.concat(b);
+    }
+    else if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }
+    else {
+        return ("Arguments must be of the same type and either strings or numbers.");
+    }
 };
-
 console.log(concatSum("Hello, ", "world!")); // Output: "Hello, world!"
 console.log(concatSum(5, 10)); // Output: 15
 console.log(concatSum("Hello", 5)); // Output: Arguments must be of the same type and either strings or numbers.
