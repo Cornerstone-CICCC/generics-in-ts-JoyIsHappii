42d8ffcd3a598d1b217179e8f900c7b308d5b6df
JoyIsHappii <chayaninkosol@gmail.com>
2026-04-28 14:55:18 -0700

    ex02: done

--- dist/ex02.js
+++ dist/ex02.js
@@ -1,7 +1,9 @@
 "use strict";
 // Create a generic function named `identity` that takes a single argument of type `T` and returns that argument.
 // Create a few test cases, such as using the function with numbers, strings, and custom objects to demonstrate its type flexibility.
-Object.defineProperty(exports, "__esModule", { value: true });
-const identify = () => {
-    // Code here
+const identify = (arg) => {
+    return arg;
 };
+console.log(identify(12)); // Output: 12
+console.log(identify("Hello, World!")); // Output: "Hello, World!"
+console.log(identify({ name: "Alice", age: 30 })); // Output: { name: "Alice", age: 30 }
--- exercises/ex02.ts
+++ exercises/ex02.ts
@@ -1,6 +1,10 @@
 // Create a generic function named `identity` that takes a single argument of type `T` and returns that argument.
 // Create a few test cases, such as using the function with numbers, strings, and custom objects to demonstrate its type flexibility.
 
-const identify = () => {
-  // Code here
+const identify = <T>(arg: T): T => {
+  return arg;
 };
+
+console.log(identify(12)); // Output: 12
+console.log(identify("Hello, World!")); // Output: "Hello, World!"
+console.log(identify({ name: "Alice", age: 30 })); // Output: { name: "Alice", age: 30 }
