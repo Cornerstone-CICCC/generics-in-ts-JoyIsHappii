57bcd33743ebb21c8c57335d2cbf8c53a9a36352
JoyIsHappii <chayaninkosol@gmail.com>
2026-04-28 15:13:52 -0700

    ex06: done

--- dist/ex06.js
+++ dist/ex06.js
@@ -1,7 +1,9 @@
 "use strict";
 // Define a generic function named `createPair` that takes two arguments of types `A` and `B` and returns them as a pair within an array `[A, B]`.
 // Provide test cases using different types for `A` and `B` parameters.
-Object.defineProperty(exports, "__esModule", { value: true });
-const createPair = () => {
-    // Code here
+const createPair = (a, b) => {
+    return [a, b];
 };
+console.log(createPair(1, 2)); // Output: [1, 2]
+console.log(createPair('a', 'b')); // Output: ['a', 'b']
+console.log(createPair(1, 'a')); // Output: [1, 'a']
--- exercises/ex06.ts
+++ exercises/ex06.ts
@@ -1,6 +1,11 @@
 // Define a generic function named `createPair` that takes two arguments of types `A` and `B` and returns them as a pair within an array `[A, B]`.
 // Provide test cases using different types for `A` and `B` parameters.
 
-const createPair = () => {
-  // Code here
+const createPair = <A, B>(a: A, b: B): [A, B] => {
+  return [a, b];
 };
+
+console.log(createPair(1, 2)); // Output: [1, 2]
+console.log(createPair('a', 'b')); // Output: ['a', 'b']
+console.log(createPair(1, 'a')); // Output: [1, 'a']
+
